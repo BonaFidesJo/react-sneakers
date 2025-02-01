5bd92fc81c9b80b8f794189da75ba6573507116b
Joanna Agupova <hannah.pinkman@gmail.com>
2025-02-01 12:47:18 +0300

    com

--- src/js/react/components/Card.jsx
+++ src/js/react/components/Card.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 
 
-const Card = () => {
+const Card = (props) => {
+
+
 	return (
 		<div className="card">
-			<div className="card__like"> <img src="@img/card/heart-01.svg" alt="unlike" /></div>
-			<img className="card__image" src="@img/card/sneakers/01.jpg" alt="Image" />
+			<div className="card__like" onClick={props.onFavourite} > <img src="@img/card/heart-01.svg" alt="unlike" /></div>
+			<img className="card__image" src={props.image} alt="Image" />
 			<div className="card__name">
-				Мужские Кроссовки Nike Blazer Mid Suede
+				{props.title}
 			</div>
 			<div className="card__bottom">
 				<div className="card__price">
 					<div className="card__subtitle">Цена:</div>
-					<div className="card__amout">12 999 руб.</div>
+					<div className="card__amout">{props.price} руб.</div>
 				</div>
-				<button className="card__pluse">
+				<button className="card__pluse" onClick={props.onPluse} >
 					<img src="@img/card/pluse-01.svg" alt="" className="card__pluse-img" />
 				</button>
 			</div>
